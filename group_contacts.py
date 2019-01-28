1ee5b53d0610c5bafe906258a4705cfb764a5d05
klnrst <kseniyakalenik@gmail.com>
2019-01-27 20:23:00 -0600

    Group Class created

---
+++ group_contacts.py
@@ -0,0 +1,14 @@
+class Group:
+    def __init__(self, firstname, lastname, title, company, address, phonenumber, email, bday, bmonth,
+                           byear, notes):
+        self.firstname = firstname
+        self.lastname = lastname
+        self.title = title
+        self.company = company
+        self.address = address
+        self.phonenumber = phonenumber
+        self.email = email
+        self.bday = bday
+        self.bmonth = bmonth
+        self.byear = byear
+        self.notes = notes
